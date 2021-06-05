6122cdf32ae6cf9d641d3aef62e642cabafdcac5
ugurkrl <ugurkrl@outlook.com>
2021-06-06 01:35:29 +0300

    .

--- SW/SW.ino
+++ SW/SW.ino
@@ -1,99 +1,142 @@
 #include <U8g2lib.h>
-
+#include <ELECHOUSE_CC1101_SRC_DRV.h>
 bool matrix1[2];
 bool matrix2[2];
-char keys1[] = {'B','C'};
-char keys2[] = {'A','D'};
+char keys1[] = {'B', 'C'};
+char keys2[] = {'A', 'D'};
 int packetbuffer[20];
 int bufferindex;
 int latchtime = 100;
-bool latch=false;
+bool latch = false;
 unsigned long lastlatch = 0;
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C oled(U8G2_R0);
 
-void setup(){
+void setup() {
   Serial.begin(9600);
-  pinMode(A0,OUTPUT);
-  pinMode(A1,OUTPUT);
-  pinMode(8,INPUT_PULLUP);
-  pinMode(9,INPUT_PULLUP);
-  pinMode(2,INPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(3, INPUT);
   oled.begin();
-  oled.setCursor(0,16);
-  attachInterrupt(0,sendbuffer,RISING); 
+  attachInterrupt(1, sendbuffer, RISING);
+  cc1101set();
+
+
 
 }
 
-void loop(){
-    oled.clearBuffer();
-   oled.setCursor(0,16);
-    oled.setDrawColor(1);
-    oled.setFont(u8g2_font_helvB10_tr);
+void loop() {
+  oled.clearBuffer();
+  oled.setCursor(0, 16);
+  oled.setFont(u8g2_font_helvB10_tr);
+  scankey(matrix1 , matrix2);
+  printbuffer();
 
-scankey(matrix1 , matrix2);
-printbuffer();
-
- for(int i = 0; i < 20; i++)
-{
-  oled.print((char)packetbuffer[i]);
-}
+  for (int i = 0; i < 20; i++)
+  {
+    oled.print((char)packetbuffer[i]);
+  }
 
   oled.sendBuffer();
+
 }
 
-void scankey(bool *m1,bool *m2){
+void scankey(bool *m1, bool *m2) {
 
-digitalWrite(A0,LOW);
-digitalWrite(A1,HIGH);
-m1[0]=digitalRead(8);
-m1[1]=digitalRead(9);
-digitalWrite(A0,HIGH);
-digitalWrite(A1,LOW);
-m2[0]=digitalRead(8);
-m2[1]=digitalRead(9);
+  digitalWrite(A0, LOW);
+  digitalWrite(A1, HIGH);
+  m1[0] = digitalRead(8);
+  m1[1] = digitalRead(9);
+  digitalWrite(A0, HIGH);
+  digitalWrite(A1, LOW);
+  m2[0] = digitalRead(8);
+  m2[1] = digitalRead(9);
 }
 
-void printbuffer(){
-if(latch == false){
-    for (int i=0; i<2; i++)  
+void printbuffer() {
+  if (latch == false) {
+    for (int i = 0; i < 2; i++)
     {
-    if(matrix1[i]==0){
-      packetbuffer[bufferindex] = keys1[i];
-      bufferindex = bufferindex + 1;
-      if(bufferindex >= 20){bufferindex = 20;}
+      if (matrix1[i] == 0) {
+        packetbuffer[bufferindex] = keys1[i];
+        bufferindex = bufferindex + 1;
+        if (bufferindex >= 20) {
+          bufferindex = 20;
+        }
 
-      latch=true;
-      lastlatch = millis();
+        latch = true;
+        lastlatch = millis();
+      }
     }
-    }
-    
-    for (int i=0; i<2; i++)  
+
+    for (int i = 0; i < 2; i++)
     {
-    if(matrix2[i]==0){
-      packetbuffer[bufferindex] = keys2[i];
-      bufferindex = bufferindex + 1;
-      if(bufferindex >= 20){bufferindex = 20;}
-      
-      latch=true;
-      lastlatch = millis();
+      if (matrix2[i] == 0) {
+        packetbuffer[bufferindex] = keys2[i];
+        bufferindex = bufferindex + 1;
+        if (bufferindex >= 20) {
+          bufferindex = 20;
+        }
+
+        latch = true;
+        lastlatch = millis();
+      }
     }
-    }
-}
-if(latch == true && (millis() >= lastlatch + latchtime ) ){
+  }
+  if (latch == true && (millis() >= lastlatch + latchtime ) ) {
     latch = false;
-}
+  }
 }
 
-void sendbuffer(){   //butona basıldığında buffer aktar
-  for(int i = 0; i < 20; i++)
-{
-  Serial.print(packetbuffer[i]);
+void sendbuffer() {  //butona basıldığında buffer aktar
+  for (int i = 0; i < 20; i++)
+  {
+    Serial.print(packetbuffer[i]);
+  }
+  ELECHOUSE_cc1101.SendData((int)packetbuffer,20 );
+  for (int i = 0; i < 20; i++)
+  {
+    packetbuffer[i] = 0;
+  }
+  bufferindex = 0;
+  Serial.println();
+
+
 }
-    for(int i = 0; i < 20; i++)
-{
-  packetbuffer[i]=0;
-  
-}
-bufferindex = 0;
-Serial.println();
+
+
+
+void cc1101set() {
+  ELECHOUSE_cc1101.Init();                // must be set to initialize the cc1101!
+  ELECHOUSE_cc1101.setCCMode(1);          // set config for internal transmission mode.
+  ELECHOUSE_cc1101.setModulation(0);      // set modulation mode. 0 = 2-FSK, 1 = GFSK, 2 = ASK/OOK, 3 = 4-FSK, 4 = MSK.
+  ELECHOUSE_cc1101.setMHZ(433.92);        // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
+  ELECHOUSE_cc1101.setDeviation(47.60);   // Set the Frequency deviation in kHz. Value from 1.58 to 380.85. Default is 47.60 kHz.
+  ELECHOUSE_cc1101.setChannel(0);         // Set the Channelnumber from 0 to 255. Default is cahnnel 0.
+  ELECHOUSE_cc1101.setChsp(199.95);       // The channel spacing is multiplied by the channel number CHAN and added to the base frequency in kHz. Value from 25.39 to 405.45. Default is 199.95 kHz.
+  ELECHOUSE_cc1101.setRxBW(812.50);       // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.
+  ELECHOUSE_cc1101.setDRate(99.97);       // Set the Data Rate in kBaud. Value from 0.02 to 1621.83. Default is 99.97 kBaud!
+  ELECHOUSE_cc1101.setPA(10);             // Set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12) Default is max!
+  ELECHOUSE_cc1101.setSyncMode(2);        // Combined sync-word qualifier mode. 0 = No preamble/sync. 1 = 16 sync word bits detected. 2 = 16/16 sync word bits detected. 3 = 30/32 sync word bits detected. 4 = No preamble/sync, carrier-sense above threshold. 5 = 15/16 + carrier-sense above threshold. 6 = 16/16 + carrier-sense above threshold. 7 = 30/32 + carrier-sense above threshold.
+  ELECHOUSE_cc1101.setSyncWord(211, 145); // Set sync word. Must be the same for the transmitter and receiver. (Syncword high, Syncword low)
+  ELECHOUSE_cc1101.setAdrChk(0);          // Controls address check configuration of received packages. 0 = No address check. 1 = Address check, no broadcast. 2 = Address check and 0 (0x00) broadcast. 3 = Address check and 0 (0x00) and 255 (0xFF) broadcast.
+  ELECHOUSE_cc1101.setAddr(0);            // Address used for packet filtration. Optional broadcast addresses are 0 (0x00) and 255 (0xFF).
+  ELECHOUSE_cc1101.setWhiteData(0);       // Turn data whitening on / off. 0 = Whitening off. 1 = Whitening on.
+  ELECHOUSE_cc1101.setPktFormat(0);       // Format of RX and TX data. 0 = Normal mode, use FIFOs for RX and TX. 1 = Synchronous serial mode, Data in on GDO0 and data out on either of the GDOx pins. 2 = Random TX mode; sends random data using PN9 generator. Used for test. Works as normal mode, setting 0 (00), in RX. 3 = Asynchronous serial mode, Data in on GDO0 and data out on either of the GDOx pins.
+  ELECHOUSE_cc1101.setLengthConfig(1);    // 0 = Fixed packet length mode. 1 = Variable packet length mode. 2 = Infinite packet length mode. 3 = Reserved
+  ELECHOUSE_cc1101.setPacketLength(0);    // Indicates the packet length when fixed packet length mode is enabled. If variable packet length mode is used, this value indicates the maximum packet length allowed.
+  ELECHOUSE_cc1101.setCrc(1);             // 1 = CRC calculation in TX and CRC check in RX enabled. 0 = CRC disabled for TX and RX.
+  ELECHOUSE_cc1101.setCRC_AF(0);          // Enable automatic flush of RX FIFO when CRC is not OK. This requires that only one packet is in the RXIFIFO and that packet length is limited to the RX FIFO size.
+  ELECHOUSE_cc1101.setDcFilterOff(0);     // Disable digital DC blocking filter before demodulator. Only for data rates ≤ 250 kBaud The recommended IF frequency changes when the DC blocking is disabled. 1 = Disable (current optimized). 0 = Enable (better sensitivity).
+  ELECHOUSE_cc1101.setManchester(0);      // Enables Manchester encoding/decoding. 0 = Disable. 1 = Enable.
+  ELECHOUSE_cc1101.setFEC(0);             // Enable Forward Error Correction (FEC) with interleaving for packet payload (Only supported for fixed packet length mode. 0 = Disable. 1 = Enable.
+  ELECHOUSE_cc1101.setPRE(0);             // Sets the minimum number of preamble bytes to be transmitted. Values: 0 : 2, 1 : 3, 2 : 4, 3 : 6, 4 : 8, 5 : 12, 6 : 16, 7 : 24
+  ELECHOUSE_cc1101.setPQT(0);             // Preamble quality estimator threshold. The preamble quality estimator increases an internal counter by one each time a bit is received that is different from the previous bit, and decreases the counter by 8 each time a bit is received that is the same as the last bit. A threshold of 4∙PQT for this counter is used to gate sync word detection. When PQT=0 a sync word is always accepted.
+  ELECHOUSE_cc1101.setAppendStatus(0);    // When enabled, two status bytes will be appended to the payload of the packet. The status bytes contain RSSI and LQI values, as well as CRC OK.
+  if (ELECHOUSE_cc1101.getCC1101()) {     // Check the CC1101 Spi connection.
+    Serial.println("Connection OK");
+  } else {
+    Serial.println("Connection Error");
+  }
 }
